15d2897836a4e90aadd556b103bb95f03b1b29c5
ocean <contest.start@gmail.com>
2020-08-26 23:18:06 +0800

    add example

--- referentialTransparency/ex1.js
+++ referentialTransparency/ex1.js
@@ -2,7 +2,7 @@ var a = 2;
 var b = 1;
 const add = (a, b)=> a + b;
 
+console.log('2 + 1 = ' + (2 + 1));
 console.log('2 + 1 = ' + add(a, b));
-console.log('2 + 1 = ' + add(a, b));
-console.log('2 + 1 = ' + add(a, b));
+console.log('2 + 1 = ' + add(2, 1));
 console.log('2 + 1 = ' + add(a, b));
--- referentialTransparency/ex2.js
+++ referentialTransparency/ex2.js
@@ -1,8 +1,12 @@
 var a = 2;
 var b = 1;
-const add = (b)=> ++a + b;
 
+const add = (x, y)=> {
+    a += 1;
+    return x + y
+};
+
+console.log('2 + 1 = ' + (2 + 1));
 console.log('2 + 1 = ' + add(a, b));
-console.log('2 + 1 = ' + add(a, b));
-console.log('2 + 1 = ' + add(a, b));
+console.log('2 + 1 = ' + add(2, 1));
 console.log('2 + 1 = ' + add(a, b));
